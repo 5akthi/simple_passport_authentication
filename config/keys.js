4cdd36b666f01ea9fd237b7230d39e14c9a69ad4
Sakthi Vigneshvar <sakthivicky41@gmail.com>
2020-06-24 19:41:55 +0400

    Initial commit

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+  MongoURI:
+    "mongodb+srv://5akthi:vicky@123@testcluster1-ptfmk.mongodb.net/test?retryWrites=true&w=majority",
+};
+
+// Please feel free to use your own MongoURI key, which you can get it from MongoDB Atlas.
