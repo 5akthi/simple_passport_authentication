be4f41d3d0b45863ee4f256819786fe23bd1a03b
Sakthi Vigneshvar <sakthivicky41@gmail.com>
2020-09-04 11:18:24 +0400

    Changed Mongo URI

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,5 @@
 module.exports = {
-  MongoURI:
-    "mongodb+srv://5akthi:vicky@123@testcluster1-ptfmk.mongodb.net/test?retryWrites=true&w=majority",
+  MongoURI: "YOUR_MONGO_URI",
 };
 
 // Please feel free to use your own MongoURI key, which you can get it from MongoDB Atlas.
